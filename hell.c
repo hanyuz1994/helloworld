b1dc380181aebe24a4e7e4333b20ef276870adaf
zhanghanyu <zhanghanyu@bbktel.com>
2017-11-19 12:32:23 +0800

    patch hello.c

--- hell.c
+++ hell.c
@@ -10,15 +10,18 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
+ *         Author:  YOUR NAME (patch), 
  *        Company:  
  *
  * =====================================================================================
  */
 
+this is a patch
 hi boy:
 hi girl:
 world
+this is a patch
 so what 
 f**k
 lala
+this is a patch
